b69ad2cca50ac90c00396ad3288ce81b31325d34
Quang <vuquangit@gmail.com>
2019-12-23 10:30:27 +0700

    feat: debug router deploy

--- src/Redux/Auth/auth.reducer.js
+++ src/Redux/Auth/auth.reducer.js
@@ -1,6 +1,6 @@
 import { SET_AUTHENTICATED } from "./actionType";
 
-const initialState = { isAuthenticated: false };
+const initialState = { isAuthenticated: true };
 
 const todos = (state = initialState, action) => {
   switch (action.type) {
