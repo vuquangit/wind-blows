ff53476c4b94ad614462d17ee06c8b371eec456b
vuquangit <vuquangit@gmail.com>
2023-01-31 23:41:08 +0700

    feat: add typescript

--- src/index.tsx
+++ src/index.tsx
@@ -30,25 +30,29 @@ store.subscribe(
 )
 
 // dev tool
-if (process.env.NODE_ENV !== 'development') {
-  if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === 'object') {
-    for (const [key, value] of Object.entries(
-      window.__REACT_DEVTOOLS_GLOBAL_HOOK__
-    )) {
-      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[key] =
-        typeof value === 'function' ? () => {} : null
-    }
-  }
+// if (process.env.NODE_ENV !== 'development') {
+//   if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === 'object') {
+//     for (const [key, value] of Object.entries(
+//       window.__REACT_DEVTOOLS_GLOBAL_HOOK__
+//     )) {
+//       window.__REACT_DEVTOOLS_GLOBAL_HOOK__[key] =
+//         typeof value === 'function' ? () => {} : null
+//     }
+//   }
+// }
+
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <CloudinaryContext cloudName={cloudinaryConfig.cloud_name}>
+        <App />
+      </CloudinaryContext>
+    </Provider>,
+    document.getElementById('root')
+  )
 }
 
-ReactDOM.render(
-  <Provider store={store}>
-    <CloudinaryContext cloudName={cloudinaryConfig.cloud_name}>
-      <App />
-    </CloudinaryContext>
-  </Provider>,
-  document.getElementById('root')
-)
+render()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
